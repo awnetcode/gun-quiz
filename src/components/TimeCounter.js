750d27e49ef3810fcfcb5821ba0fae2b2ae98b14
awnetcode <155958835+awnetcode@users.noreply.github.com>
2025-04-17 12:21:53 +0200

    feedback-elements

---
+++ src/components/TimeCounter.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+import { Box, Typography } from '@mui/material';
+
+const TimeCounter = () => {
+    const [secondsLeft, setSecondsLeft] = useState(20 * 60); // 20 minut
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setSecondsLeft(prev => {
+          if (prev > 0) {
+            return prev - 1;
+          } else {
+            clearInterval(interval);
+            return 0;
+          }
+        });
+      }, 1000);
+  
+      // Czyścimy interwał, gdy komponent się unmountuje
+      return () => clearInterval(interval);
+    }, []);
+  
+    // Obliczamy minuty i sekundy do wyświetlenia
+    const minutes = Math.floor(secondsLeft / 60);
+    const seconds = secondsLeft % 60;
+  
+    // Formatowanie do 00:00
+    const formatTime = (time) => (time < 10 ? `0${time}` : time);
+  
+
+  return (
+    <Box>
+        <Typography className='time-counter' sx={{
+          background:'var(--seasalt)',
+          fontFamily: 'Kode Mono, monospace', 
+          fontSize: '2rem',
+          color:'#1976d2',
+          p:'0 24px',
+          borderRadius:'4px',
+          boxShadow: '0 1px 4px 1px #0F1A20', 
+          textAlign:'center',
+          position:'absolute',
+          top: '24px',
+          right: '48px'
+        }}
+        >
+        {formatTime(minutes)}:{formatTime(seconds)}
+        </Typography>
+    </Box>
+  )
+}
+
+export default TimeCounter
--- src/components/pages/ExamMode.js
+++ src/components/pages/ExamMode.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-import { Box, Typography, Checkbox } from '@mui/material';
+import { Box, Typography, Checkbox, Skeleton, LinearProgress } from '@mui/material';
 
-
+import TimeCounter from '../TimeCounter';
 
 const ExamMode = ({questions, questionCount, setQuestionCount}) => {
 
   const [randomQuestions, setRandomQuestions] = useState([]);
-  setQuestionCount(1)
 
   const getQuestionSet = (data, count) => {
     const shuffled = [...data].sort(() => Math.random() - 0.5);
@@ -18,16 +17,33 @@ const ExamMode = ({questions, questionCount, setQuestionCount}) => {
   useEffect(() => {
     const selectedQuestions = getQuestionSet(questions, 20);
     setRandomQuestions(selectedQuestions);
-  }, [randomQuestions])
+    setQuestionCount(0);
+
+  }, [])
+
+  if (!randomQuestions[questionCount]) {
+    return(<>
+    <Typography>Ładowanie...</Typography>
+    <Skeleton variant='text' width={500} height={120}/>
+    <Skeleton variant='text' width={500} height={60}/>
+    <Skeleton variant='text' width={500} height={60}/>
+    <Skeleton variant='text' width={500} height={60}/>
+    </>)
+   // return <Typography>Ładowanie pytań...</Typography>;
+  }
 
   return (
+    <>
+    <Box><TimeCounter/></Box>
     <Box sx={{
       fontSize:'24px',
     }}>
     <Typography sx={{
       textAlign:'center',
       fontSize: '24px',
-    }}>Pytanie  {randomQuestions[questionCount].number} ({randomQuestions[questionCount].hint === '' ? ' brak ' : randomQuestions[questionCount].hint})
+      fontFamily: 'Cormorant-Garamond, serif'
+    }}>Pytanie  {randomQuestions[questionCount].number} <br/>
+    ({randomQuestions[questionCount].hint === '' ? ' brak ' : randomQuestions[questionCount].hint})
     </Typography>
     <Typography sx={{
       fontSize:'24px',
@@ -39,15 +55,32 @@ const ExamMode = ({questions, questionCount, setQuestionCount}) => {
     }}
     >{randomQuestions[questionCount].question}</Typography>
     <Typography sx={{fontSize:'20px', ml:'40px', p:'4px'}}>
+      <Checkbox/>
     <span className='question-key'> a. </span> 
     {randomQuestions[questionCount].answers.a}</Typography>
     <Typography sx={{fontSize:'20px', ml:'40px', p:'4px'}}>
+      <Checkbox/>
     <span className='question-key'> b. </span> 
     {randomQuestions[questionCount].answers.b}</Typography>
     <Typography sx={{fontSize:'20px', ml:'40px', p:'4px'}}>
+      <Checkbox/>
     <span className='question-key'> c. </span> 
     {randomQuestions[questionCount].answers.c}</Typography>
   </Box>
+  <Box sx={{
+        width:'60%',
+        position:'absolute',
+        bottom:'100px',
+        left:'20%',
+        fontFamily:'Kode Mono'
+  }}>
+  <LinearProgress sx={{
+    height:'5px',
+
+  }}
+   color='success' variant="determinate" value={questionCount*5 + 5}/> {questionCount +1} / 20
+  </Box>
+  </>
   )
 }
 
