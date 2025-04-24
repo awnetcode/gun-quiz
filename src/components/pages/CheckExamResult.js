5849bc247fe61bafe269aaf3b4be6403875045da
awnetcode <155958835+awnetcode@users.noreply.github.com>
2025-04-24 21:40:37 +0200

    exam-result-feedback

--- src/components/pages/CheckExamResult.js
+++ src/components/pages/CheckExamResult.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useEffect } from 'react';
 
-import { Box, Typography} from '@mui/material'
+import { Box, Typography} from '@mui/material';
+//import CancelIcon from '@mui/icons-material/Cancel';
+//import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import { Cancel, CheckCircleOutline } from '@mui/icons-material';
+
 
 const CheckExamResult = ({
     examResult,
@@ -14,6 +18,7 @@ useEffect(() =>{
 setQuestionCount(0);
 setListLength(examResult.length)
 console.log(examResult)
+// eslint-disable-next-line react-hooks/exhaustive-deps
 }, []);
 
   return (
@@ -34,15 +39,51 @@ console.log(examResult)
       textAlign:'justify'
     }}
     >{examResult[questionCount].question}</Typography>
-    <Typography sx={{fontSize:'20px', ml:'40px', p:'4px'}}>
-    <span className='question-key'> a. </span> 
-    {examResult[questionCount].answers.a}</Typography>
-    <Typography sx={{fontSize:'20px', ml:'40px', p:'4px'}}>
-    <span className='question-key'> b. </span> 
-    {examResult[questionCount].answers.b}</Typography>
-    <Typography sx={{fontSize:'20px', ml:'40px', p:'4px'}}>
-    <span className='question-key'> c. </span> 
-    {examResult[questionCount].answers.c}</Typography>
+    {Object.entries(examResult[questionCount].answers).map(([key, contents]) =>(
+      <Typography key={key} sx={{
+        fontSize:'20px', 
+        ml:'40px', 
+        p:'4px'
+      }}>
+      {key === examResult[questionCount].markedAnswer && 
+      key === examResult[questionCount].correctAnswer ? 
+        <CheckCircleOutline sx={{
+          color: 'green',
+          fontSize: '36px',
+          transform: 'translate(-36px, 10px)',
+        }}/>
+         : ''}
+      {key === examResult[questionCount].markedAnswer && 
+      key !== examResult[questionCount].correctAnswer ? 
+      <> 
+      <Cancel sx={{
+        color: 'red',
+        fontSize: '36px',
+        transform: 'translate(-36px, 10px)',
+      }}/>
+    <span className='correct-answer-info'>
+      Prawidłowa odpowiedź: {examResult[questionCount].correctAnswer}
+    </span>
+      </>
+         : ''}
+      {examResult[questionCount].markedAnswer === '' &&
+      key === examResult[questionCount].correctAnswer ? 
+      <>
+      <CheckCircleOutline sx={{
+        color: 'gray',
+        fontSize: '36px',
+        transform: 'translate(-36px, 10px)',
+      }}/>
+          <span className='correct-answer-info'>
+      Brak odpowiedzi! <br/>
+      Prawidłowa odpowiedź: {examResult[questionCount].correctAnswer}
+    </span>
+      </>
+       : ''}
+        <span className='question-key'>{key}. </span>
+        {contents}
+      </Typography>
+    ))}
   </Box>
   )
 }
